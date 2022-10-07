8ebae17760d7727edd3ec1d12d7ff20787b6b877
RafaelCENG <rnicolaouceng@gmail.com>
2022-10-07 13:20:12 +0300

    Personal Portfolio Projects,Project-Card

--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -2,12 +2,16 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 import { Banner } from './components/Banner.jsx'
 import { NavBar } from './components/NavBar.jsx'
+import { Projects } from './components/Projects.jsx'
+import { Skills } from './components/Skills.jsx'
 
 function App() {
 	return (
 		<div className="App">
 			<NavBar />
 			<Banner />
+			<Skills />
+			<Projects />
 		</div>
 	)
 }
