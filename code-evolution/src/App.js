04562b85a0b01240d3f2edc943f9be3be49dd590
RafaelCENG <rnicolaouceng@gmail.com>
2022-11-09 15:07:33 +0200

    CE

--- code-evolution/src/App.js
+++ code-evolution/src/App.js
@@ -8,6 +8,8 @@ import CounterOneReducer from './components/CounterOneReducer'
 import CounterThreeReducer from './components/CounterThreeReducer'
 import CounterTwoReducer from './components/CounterTwoReducer'
 import DataFetching from './components/DataFetching'
+import DataFetching1 from './components/DataFetching1'
+import DataFetching2 from './components/DataFetching2'
 import HookCounter from './components/HookCounter'
 import HookCounter2 from './components/HookCounter2'
 import HookCounter3 from './components/HookCounter3'
@@ -15,38 +17,15 @@ import HookCounter4 from './components/HookCounter4'
 import HookCounterOne from './components/HookCounterOne'
 import HookMouse from './components/HookMouse'
 import IntervalHookCounter from './components/IntervalHookCounter'
+import MemoHook from './components/MemoHook'
 import MouseContainer from './components/MouseContainer'
-
-export const CountContext = createContext()
-
-const initialState = 0
-const reducer = (state, action) => {
-	switch (action) {
-		case 'increment':
-			return state + 1
-		case 'decrement':
-			return state + -1
-		case 'reset':
-			return initialState
-
-		default:
-			return state
-	}
-}
+import ParentComponent from './components/ParentComponent'
 
 function App() {
-	const [count, dispatch] = useReducer(reducer, initialState)
 	return (
-		<CountContext.Provider
-			value={{ countState: count, countDispatch: dispatch }}
-		>
-			<div className="App">
-				Count - {count}
-				<ComponentAreducer />
-				<ComponentBreducer />
-				<ComponentCreducer />
-			</div>
-		</CountContext.Provider>
+		<div>
+			<MemoHook />
+		</div>
 	)
 }
 
