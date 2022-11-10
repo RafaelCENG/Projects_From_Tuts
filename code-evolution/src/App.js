4239caf996992833f89a238145882e976bb4f9e9
RafaelCENG <rnicolaouceng@gmail.com>
2022-11-10 16:10:59 +0200

    Finished Tutorials of Hooks

--- code-evolution/src/App.js
+++ code-evolution/src/App.js
@@ -4,27 +4,34 @@ import ComponentAreducer from './components/ComponentAreducer'
 import ComponentBreducer from './components/ComponentBreducer'
 import ComponentC from './components/ComponentC'
 import ComponentCreducer from './components/ComponentCreducer'
+import CounterOne from './components/CounterOne'
 import CounterOneReducer from './components/CounterOneReducer'
 import CounterThreeReducer from './components/CounterThreeReducer'
+import CounterTwo from './components/CounterTwo'
 import CounterTwoReducer from './components/CounterTwoReducer'
 import DataFetching from './components/DataFetching'
 import DataFetching1 from './components/DataFetching1'
 import DataFetching2 from './components/DataFetching2'
+import DocTitleOne from './components/DocTitleOne'
+import DocTitleTwo from './components/DocTitleTwo'
+import FocusInput from './components/FocusInput'
 import HookCounter from './components/HookCounter'
 import HookCounter2 from './components/HookCounter2'
 import HookCounter3 from './components/HookCounter3'
 import HookCounter4 from './components/HookCounter4'
 import HookCounterOne from './components/HookCounterOne'
 import HookMouse from './components/HookMouse'
+import HookTimer from './components/HookTimer'
 import IntervalHookCounter from './components/IntervalHookCounter'
 import MemoHook from './components/MemoHook'
 import MouseContainer from './components/MouseContainer'
 import ParentComponent from './components/ParentComponent'
+import UserForm from './components/UserForm'
 
 function App() {
 	return (
 		<div>
-			<MemoHook />
+			<UserForm />
 		</div>
 	)
 }
