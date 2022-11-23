a42a0da72e2b365e710dd36a34179d46aea4a3cd
RafaelCENG <rnicolaouceng@gmail.com>
2022-11-23 15:16:27 +0200

    Redux Tutorial Done

---
+++ react-rtk-ts-demo/src/features/icecream/IcecreamView.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import { useAppDispatch, useAppSelector } from '../../app/hooks'
+import { ordered, restocked } from './icecreamSlice'
+
+const IcecreamView = () => {
+	const [value, setValue] = useState(1)
+
+	const numOfCakes = useAppSelector((state) => state.icecream.numOfIcecreams)
+	const dispatch = useAppDispatch()
+	return (
+		<div>
+			<h2>Number of Ice creams - {numOfCakes}</h2>
+			<button onClick={() => dispatch(ordered())}>Order Ice cream</button>
+			<input
+				type="number"
+				value={value}
+				onChange={(e) => setValue(parseInt(e.target.value))}
+			/>
+			<button onClick={() => dispatch(restocked(value))}>
+				Restock Ice creams
+			</button>
+		</div>
+	)
+}
+
+export default IcecreamView
